85383d2c9db470c660bfd5eb6ce09987818b2dcb
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-06-11 16:24:16 +0100

    Updated sql format for retrieving article
    New format makes retrieving articles safe and prevents malicious sql to be run on DB
    
    Co-Authored-By: Salaheldin Akl <salaheldin.akl@student.dit.ie>

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 router.get('articles/article/:id', (req, res) => {
     const db = req.app.get('db')
-    db.query('SELECT * FROM article WHERE id = ' + req.params.id, (error, results) => {
+    const sql = mysql.format('SELECT * FROM article WHERE id = ??', [req.params.id])
+    db.query(sql, (error, results) => {
 
         if (error) throw error
         res.json(results)
