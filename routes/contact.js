adbde5e968cb1960ea85263881bb39a8c1af62e5
salahusama <salaheldin.akl@student.dit.ie>
2019-06-12 11:50:25 +0100

    Add endpoint to send emails

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const transporter = require('../utils/email')
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('Contact API.'))
+router.post('/send', (req, res) => {
+    const { email, subject, text } = req.body
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject,
+        text,
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error)
+            res.status(500)
+        } else {
+            console.log('Email sent: ' + info.response)
+        }
+    })
+
+    res.end()
+})
 
 module.exports = router
