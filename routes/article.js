12ba26ba2c69d34f64a1dffdccfaabcc2a986717
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-06-12 13:55:19 +0100

    Update routes/article.js
    Co-Authored-By: Salaheldin Akl <salaheldin.akl@student.dit.ie>

--- routes/article.js
+++ routes/article.js
@@ -5,7 +5,6 @@ router.get('/articles/article/:id', (req, res) => {
     const db = req.app.get('db')
     const sql = mysql.format('SELECT * FROM article WHERE id = ??', [req.params.id])
     db.query(sql, (error, results) => {
-
         if (error) throw error
         res.json(results)
     })
