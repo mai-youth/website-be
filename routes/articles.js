2f839fda2970600162567b633fdb23c0f4f9788c
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-06-14 00:39:45 +0400

    Fix some merge issues from the last PR (#17)

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const mysql = require('mysql')
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -7,6 +7,20 @@ router.get('/', (req, res) => {
     db.query('Select * from articles', (err, result) => {
         if (err) throw err
         res.json(result)
+    })
+})
+
+router.get('/article/:id', (req, res) => {
+    const db = req.app.get('db')
+    const sql = mysql.format('SELECT * FROM articles WHERE id = ?', [req.params.id])
+    db.query(sql, (error, results) => {
+        if (error) throw error
+        if (results.length === 0) {
+            res.status(404)
+            res.end()
+        } else {
+            res.json(results[0])
+        }
     })
 })
 
