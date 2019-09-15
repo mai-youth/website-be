432f56533dc3430f82edbf2d3ce4bfe1223510bd
salahusama <salaheldin.akl@student.dit.ie>
2019-09-15 08:58:14 +0100

    Add additional article fields to the DB

--- schema.sql
+++ schema.sql
@@ -1,7 +1,12 @@
 CREATE TABLE articles (
-    id		INT AUTO_INCREMENT,
-    title	TEXT NOT NULL,
-    body	LONGTEXT NOT NULL,
-    author	VARCHAR(50) NOT NULL DEFAULT "MAI Youth Team",
+    id		    INT AUTO_INCREMENT,
+    title	    TEXT NOT NULL,
+    body	    LONGTEXT NOT NULL,
+    author	    VARCHAR(50) NOT NULL DEFAULT "MAI Youth Team",
+    likes       INT DEFAULT,
+    views       INT DEFAULT,
+    color       VARCHAR(20),
+    created_at  TIMESTAMP DEFAULT NOW(), 
+    updated_at  TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
     PRIMARY KEY (id)
 );
