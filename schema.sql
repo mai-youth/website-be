f84b74a5a1e6aff777d47aa2d3c4df97303bf4f8
salahusama <salaheldin.akl@student.dit.ie>
2019-09-15 09:23:33 +0100

    Nit

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE articles (
-    id		    INT AUTO_INCREMENT,
-    title	    TEXT NOT NULL,
-    body	    LONGTEXT NOT NULL,
-    author	    VARCHAR(50) NOT NULL DEFAULT "MAI Youth Team",
+    id          INT AUTO_INCREMENT,
+    title       TEXT NOT NULL,
+    body        LONGTEXT NOT NULL,
+    author      VARCHAR(50) NOT NULL DEFAULT "MAI Youth Team",
     likes       INT DEFAULT 0,
     views       INT DEFAULT 0,
     color       VARCHAR(20) DEFAULT "#5e9de6",
