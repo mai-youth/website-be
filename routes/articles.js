73b4b4c72cf8b9edd28a7e4cd0f5b9581f183996
Salaheldin Akl <salaheldin.akl@gmail.com>
2019-09-28 14:31:18 +0100

    Add additional article fields to the DB (#24)
    * Add additional article fields to the DB
    
    * Add default 0 values for likes and views
    
    * Update article endpoints to update added fields

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+// Get all articles
 router.get('/', (req, res) => {
     const db = req.app.get('db')
     db.query('Select * from articles', (err, result) => {
@@ -9,6 +10,7 @@ router.get('/', (req, res) => {
     })
 })
 
+// Get one article by Id
 router.get('/article/:id', (req, res) => {
     const db = req.app.get('db')
     db.query('SELECT * FROM articles WHERE id = ?', [req.params.id], (error, results) => {
@@ -22,16 +24,18 @@ router.get('/article/:id', (req, res) => {
     })
 })
 
+// Add new article
 router.put('/article', (req, res) => {
     const db = req.app.get('db')
-    const { title, body, author } = req.body
+    const { title, body, author, color } = req.body
 
-    db.query('INSERT INTO articles (title, body, author) VALUES (?, ?, ?)', [title, body, author], (err, result) => {
+    db.query('INSERT INTO articles (title, body, author, color) VALUES (?, ?, ?, ?)', [title, body, author, color], (err, result) => {
         if (err) throw err
         res.json({ insertId: result.insertId })
     })
 })
 
+// Edit article
 router.post('/article/:id', (req, res) => {
     const db = req.app.get('db')
     const { id } = req.params
@@ -41,11 +45,11 @@ router.post('/article/:id', (req, res) => {
             res.status(404)
             res.end()
         } else {
-            const { title, body, author } = results[0]
-            const { newTitle, newBody, newAuthor } = req.body
+            const { title, body, author, color } = results[0]
+            const { newTitle, newBody, newAuthor, newColor } = req.body
             db.query(
-                'UPDATE articles SET title = ?, body = ?, author = ? WHERE id = ?'
-                , [newTitle || title, newBody || body, newAuthor || author, id]
+                'UPDATE articles SET title = ?, body = ?, author = ?, color = ? WHERE id = ?'
+                , [newTitle || title, newBody || body, newAuthor || author, newColor || color, id]
                 , (error) => {
                     if (error) throw error
                     res.end()
@@ -55,11 +59,32 @@ router.post('/article/:id', (req, res) => {
     })
 })
 
+// Delete article
 router.delete('/article/:id', (req, res) => {
     const db = req.app.get('db')
     db.query('DELETE FROM articles WHERE id = ?', [req.params.id], (error) => {
         if (error) throw error
         res.end()
+    })
+})
+
+// Increment article view count
+router.post('/article/:id/seen', (req, res) => {
+    const db = req.app.get('db')
+    const { id } = req.params
+    db.query('UPDATE articles SET views = views + 1 WHERE id = ?', [id], (error) => {
+        if (error) throw error
+        res.json()
+    })
+})
+
+// Increment article like count
+router.post('/article/:id/liked', (req, res) => {
+    const db = req.app.get('db')
+    const { id } = req.params
+    db.query('UPDATE articles SET likes = likes + 1 WHERE id = ?', [id], (error) => {
+        if (error) throw error
+        res.json()
     })
 })
 
